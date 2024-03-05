448b6102683b39f591693556e2a964601274da22
Reza Husain <84816920+rezahusain@users.noreply.github.com>
2024-03-05 17:41:49 +0000

    Finished product :)

--- app.rb
+++ app.rb
@@ -1,9 +1,33 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "json"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:home)
+end
+
+get("/results") do
+  #Grab the ID and Key for API usage
+  seatgeek_id = ENV.fetch("SEATGEEK_ID")
+  seatgeek_key = ENV.fetch("SEATGEEK_KEY")
+
+  #Grabs the string the user inputted
+  @result = params["item"]
+
+  #Replaces whitespace within that string with dashes
+  @result = @result.gsub(" ", "-")
+
+  #String variable which holds the api endpoint events along with authentication using the ID and Key.
+  #Also filters all the events based on the string the user inputted
+  api_url = "https://api.seatgeek.com/2/events?client_id=#{seatgeek_id}&client_secret=#{seatgeek_key}&q=#{@result}"
+
+  #Grabs data from the endpoint using HTTP, parses the data using JSON
+  raw_data = HTTP.get(api_url)
+  parsed_data = JSON.parse(raw_data)
+
+  #Stores all the events from the parsed data
+  @events = parsed_data["events"]
+  
+  erb(:results)
 end
